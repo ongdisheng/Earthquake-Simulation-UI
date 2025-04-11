e75e8667219a1068392ab0ce42f6a3b17b2c5040
ongdisheng <63136897+ongdisheng@users.noreply.github.com>
2025-04-11 17:08:06 +0800

    fix: Resolved Prettier formatting issues (#6)

--- src/services/earthquake.js
+++ src/services/earthquake.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
-const baseUrl = `${import.meta.env.VITE_DISPATCHER_API_URL}`
+import axios from "axios";
+const baseUrl = `${import.meta.env.VITE_DISPATCHER_API_URL}`;
 
-const create = newObject => {
-  return axios.post(baseUrl, newObject)
-}
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject);
+};
 
-export default { create }
+export default { create };
