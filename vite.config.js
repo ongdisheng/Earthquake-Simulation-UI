9b1396765ab54a395d9c86256d509e911ec274e0
ongdisheng <63136897+ongdisheng@users.noreply.github.com>
2025-04-12 17:31:07 +0800

    feat: Added Vitest setup (#18)

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import eslint from "vite-plugin-eslint";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./tests/setup.js",
+  },
 });
