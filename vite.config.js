f3efe243ee80afc866a3a9f1cc55a2df68dd49d5
ongdisheng <ongdisheng@gmail.com>
2025-04-13 00:43:54 +0800

    Configured CI workflow for unit tests

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
+const thresholdValue = 80;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
@@ -9,5 +11,14 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: "./tests/setup.js",
+    coverage: {
+      reporter: ["text", "json-summary", "json"],
+      thresholds: {
+        lines: thresholdValue,
+        branches: thresholdValue,
+        functions: thresholdValue,
+        statements: thresholdValue,
+      },
+    },
   },
 });
