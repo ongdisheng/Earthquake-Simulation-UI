16e44fe91ddb9e83087a3b671319a23d2e02e70e
ongdisheng <ongdisheng@gmail.com>
2025-04-27 13:57:04 +0800

    Renamed vite base URL

--- src/services/earthquake.js
+++ src/services/earthquake.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = `${import.meta.env.VITE_DISPATCHER_API_URL}`;
+const baseUrl = `${import.meta.env.VITE_BACKEND_BASE_URL}`;
 
 const create = (newObject) => {
   return axios.post(baseUrl, newObject);
