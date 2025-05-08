20688b2d65d812c323c137decd013ee192539396
ongdisheng <ongdisheng@gmail.com>
2025-05-08 19:50:17 +0800

    Added settings form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SimulationForm from "./components/SimulationForm";
+import SettingsForm from "./components/SettingsForm";
 import NavBar from "./components/NavBar";
 import PageCard from "./components/PageCard";
 
@@ -13,6 +14,14 @@ const App = () => (
         element={
           <PageCard title="Earthquake Simulator">
             <SimulationForm />
+          </PageCard>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <PageCard title="Settings">
+            <SettingsForm />
           </PageCard>
         }
       />
---
+++ src/components/SettingsForm.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import { Typography, Button, Form, InputNumber } from "antd";
+import {
+  getAlertSuppressTime,
+  updateAlertSuppressTime,
+} from "../services/settings";
+
+const { Text } = Typography;
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+const inputFieldStyle = { width: "80%" };
+
+const SettingsForm = () => {
+  const [form] = Form.useForm();
+  const [suppressTime, setSuppressTime] = useState(null);
+
+  useEffect(() => {
+    getAlertSuppressTime().then((res) => {
+      setSuppressTime(res.data.data);
+    });
+  }, []);
+
+  // update alert suppress time
+  const onFinish = (values) => {
+    updateAlertSuppressTime(values).then((res) => {
+      setSuppressTime(res.data.data);
+      form.resetFields();
+    });
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontStyle: "italic",
+          fontWeight: "bold",
+        }}
+      >
+        <Text type="secondary">Current Alert Suppress Time: </Text>
+        <Text type="success">{`${suppressTime} sec`}</Text>
+      </div>
+
+      <Form
+        {...layout}
+        form={form}
+        onFinish={onFinish}
+        style={{ marginTop: 24, maxWidth: 600 }}
+      >
+        <Form.Item
+          label="New Alert Suppress Time"
+          name="alertSuppressTime"
+          rules={[{ required: true }]}
+        >
+          <InputNumber
+            min={1}
+            style={inputFieldStyle}
+            stringMode
+            addonAfter="sec"
+          />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Update
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+
+export default SettingsForm;
