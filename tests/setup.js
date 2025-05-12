776798f6043579d30673898e4be47fad3d56fea1
ongdisheng <ongdisheng@gmail.com>
2025-05-12 13:06:41 +0800

    added websocket mock in test

--- tests/setup.js
+++ tests/setup.js
@@ -2,6 +2,19 @@ import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 
+globalThis.WebSocket = class {
+  constructor(url) {
+    this.url = url;
+    this.readyState = 1; // OPEN
+  }
+  send() {}
+  close() {
+    this.readyState = 3; // CLOSED
+  }
+  addEventListener() {}
+  removeEventListener() {}
+};
+
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
